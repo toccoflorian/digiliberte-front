fc47d934f715c9723de7ff69bc71ccca4561d2ab
“[olivierkoe]” <[olikoen@gmail.com]>
2024-06-06 14:42:33 +0200

    components structure*

---
+++ src/app/burggermenu/burggermenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-burggermenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './burggermenu.component.html',
+  styleUrl: './burggermenu.component.scss'
+})
+export class BurggermenuComponent {
+
+}
---
+++ src/app/vehicules-list/vehicules-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicules-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicules-list.component.html',
+  styleUrl: './vehicules-list.component.scss'
+})
+export class VehiculesListComponent {
+
+}
