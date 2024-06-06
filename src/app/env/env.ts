b40fea04474bc5bb88f7bb730454ed66098ea558
CedricDLMS <delmascede@gmail.com>
2024-06-06 16:56:24 +0200

    https

--- src/app/env/env.ts
+++ src/app/env/env.ts
@@ -1,14 +1,14 @@
 export const dev  = {
-    baseUrl : "http://localhost:7193/",
-    baseApiUrl : "http://localhost:7193/api",
-    motorizationUrl : "http://localhost:7193/api/Motorization",
-    brandUrl : "http://localhost:7193/Brand",
-    carPoolUrl : "http://localhost:7193/CarPool",
-    carPoolPassengerUrl : "http://localhost:7193/CarPoolPassenger",
-    categoryUrl : "http://localhost:7193/Category",
-    modelUrl : "http://localhost:7193/Model",
-    rentUrl : "http://localhost:7193/Rent",
-    userUrl : "http://localhost:7193/User",
-    vehicleUrl : "http://localhost:7193/Vehicle"
+    baseUrl : "https://localhost:7193/",
+    baseApiUrl : "https://localhost:7193/api",
+    motorizationUrl : "https://localhost:7193/api/Motorization",
+    brandUrl : "https://localhost:7193/Brand",
+    carPoolUrl : "https://localhost:7193/CarPool",
+    carPoolPassengerUrl : "https://localhost:7193/CarPoolPassenger",
+    categoryUrl : "https://localhost:7193/Category",
+    modelUrl : "https://localhost:7193/Model",
+    rentUrl : "https://localhost:7193/Rent",
+    userUrl : "https://localhost:7193/User",
+    vehicleUrl : "https://localhost:7193/Vehicle"
 
 }
