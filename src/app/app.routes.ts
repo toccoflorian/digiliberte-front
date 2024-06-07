c4cb498402b0be497b29162c23ed763b1fcf12bd
Florian <t.florian181181@gmail.com>
2024-06-07 15:12:29 +0200

    [UTIL] ajout des liens home, login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home/home.component';
+import { AuthGuardService } from '../services/authentication/auth-guard.service';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent, canActivate: [AuthGuardService]},  // canActivate: [AuthGuardService] pour rendre la page accessible aux connectés uniquement
     {path: 'login', component: LoginComponent },
 ];
 
