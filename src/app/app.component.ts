052c64e1c1fc6f0af3969532d79f2d0f80b8b90e
Florian <t.florian181181@gmail.com>
2024-06-07 14:34:56 +0200

    [ADD] Authentication & Authorization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+import { AuthGuardService } from '../services/authentication/auth-guard.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [HttpClientModule, RouterOutlet, RouterLink, RouterLinkActive],
+  providers: [AuthenticationService, AuthGuardService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CarShare-front';
   
+  constructor(){}
+
+  ngOnInit(){}
 }
