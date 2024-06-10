04d62446ed92e8f2753ce961aaa5c68da8792cab
Florian <t.florian181181@gmail.com>
2024-06-10 10:17:53 +0200

    [UTIL] Ajout commentaires explication routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,10 @@ export const routes: Routes = [
         children: [
             {path: '', component: HomePage, canActivate: [AuthGuardService]},  // canActivate: [AuthGuardService] pour rendre la page accessible aux connectés uniquement
             {path: 'login', component: LoginPageComponent },
+            // Nouvelle page (ex: rent.page.ts) ici = {path: 'rent', component: RentPageComponent, canActivate: [AuthGuardService] }
+            // Une page est un composant mais on remplace .component.ts par .page.ts et le nom de la class on met namePageComponent
+            // ensuite pour naviguer vers elle on fait (depuis le html d'un composant): <a routerLink="/rent" />
+            // Attention, c'est '/rent' dans le <a/> mais 'rent' ici dans path:
         ],
         // canActivate: [AuthGuardService]
     }
