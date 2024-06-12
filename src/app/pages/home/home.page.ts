3bc1bf3441a986e5afe85bf6b0e4af04ff95082c
CedricDLMS <delmascede@gmail.com>
2024-06-12 14:45:42 +0200

    fixing

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { VehiculesListComponent } from '../../components/vehicules-list/vehicules-list.component';
+import { GeoNavbarComponent } from '../../components/geo-navbar/geo-navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [VehiculesListComponent,GeoNavbarComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss'
 })
