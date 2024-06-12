a2ca38fc14e80d57395e9537ac0e6773da2892ac
CedricDLMS <delmascede@gmail.com>
2024-06-12 17:11:54 +0200

    creation page

--- src/app/pages/carpool/create/create.page.spec.ts
+++ src/app/pages/carpool/create/create.page.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { CreatePageComponent } from './create.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('CreatePageComponent', () => {
+  let component: CreatePageComponent;
+  let fixture: ComponentFixture<CreatePageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [CreatePageComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(CreatePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/pages/carpool/create/create.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './create.page.html',
+  styleUrl: './create.page.scss'
+})
+export class CreatePageComponent {
+
+}
