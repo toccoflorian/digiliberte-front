27502279883557beec4e1314aef857eab1783d4e
Florian <t.florian181181@gmail.com>
2024-06-12 16:06:49 +0200

    fix doublon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,8 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { VehiculesListComponent } from './components/vehicules-list/vehicules-list.component';
 import { ReservationsListComponent } from './components/reservations-list/reservations-list.component';
 import { OnInit } from '@angular/core';
-import { AuthenticationService } from '../services/authentication/authentication.service';
-import { AuthGuardService } from '../services/authentication/auth-guard.service';
-import { HttpClientModule } from '@angular/common/http';
-import { TitleboxComponent } from './components/titlebox/titlebox.component';
+import { AuthenticationService } from '../services/security/authentication.service';
+import { AuthGuardService } from '../services/security/auth-guard.service';
 
 
 @Component({
