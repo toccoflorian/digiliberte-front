6fac2776057536b6932a09ddd6ccae7311e275ca
CedricDLMS <delmascede@gmail.com>
2024-06-13 16:13:16 +0200

    ajoue fonctionnalité sur creation de covoiturage

--- src/app/components/carpool/carpool-creation/carpool-creation.component.ts
+++ src/app/components/carpool/carpool-creation/carpool-creation.component.ts
@@ -1,12 +1,55 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TitleboxComponent } from "../../titlebox/titlebox.component";
 import { GeoNavbarComponent } from "../../geo-navbar/geo-navbar.component";
+import { MatIcon } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import ICreateCarpoolRequest from "../../../../interfaces/carpool/ICreateCarpoolRequest";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-carpool-creation",
   standalone: true,
-  imports: [TitleboxComponent, GeoNavbarComponent],
+  imports: [
+    TitleboxComponent,
+    GeoNavbarComponent,
+    MatIcon,
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: "./carpool-creation.component.html",
   styleUrl: "./carpool-creation.component.scss",
 })
-export class CarpoolCreationComponent {}
+export class CarpoolCreationComponent implements OnInit {
+  requestInterface?: ICreateCarpoolRequest;
+  formGroup!: FormGroup;
+
+  // recupération des coordonnées recupérés par le geo-component
+  //#region coordonnee
+  startCoordinates: { lat: number; long: number } | null = null;
+  onStartCoordinatesSelected(coords: { lat: number; long: number }) {
+    this.startCoordinates = coords;
+    console.log(
+      "depart depuis les coordonnées suivant :",
+      this.startCoordinates
+    );
+  }
+  endCoordinates: { lat: number; long: number } | null = null;
+  onEndCoordinatesSelected(coords: { lat: number; long: number }) {
+    this.endCoordinates = coords;
+    console.log("fin aux les coordonnées suivant :", this.endCoordinates);
+  }
+  //#endregion coordonnee
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      rentId: new FormControl(""),
+      carpoolName: new FormControl(""),
+      startDate: new FormControl(""),
+      endDate: new FormControl(""),
+      startLocalization: new FormControl(""),
+      endLocalization: new FormControl(""),
+    });
+  }
+}
