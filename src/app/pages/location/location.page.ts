eb74c1eed26b22a1c789ebebaaff8b4afcc6fcdb
CedricDLMS <cdelmas@monchasseurimmo.com>
2024-10-08 14:06:21 +0200

    ajout boite titre

--- src/app/pages/location/location.page.ts
+++ src/app/pages/location/location.page.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { TitleboxComponent } from "../../components/titlebox/titlebox.component";
 
 @Component({
   selector: "app-location",
   standalone: true,
-  imports: [],
+  imports: [TitleboxComponent],
   templateUrl: "./location.page.html",
   styleUrl: "./location.page.scss",
 })
