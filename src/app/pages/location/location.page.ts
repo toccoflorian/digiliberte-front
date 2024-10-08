74b9353fcf92893cd199d6d5aa8fa08b20ef5146
CedricDLMS <cdelmas@monchasseurimmo.com>
2024-10-08 14:37:43 +0200

    suppression dossier inutiles + ajout scss + component filtre

--- src/app/pages/location/location.page.ts
+++ src/app/pages/location/location.page.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { TitleboxComponent } from "../../components/titlebox/titlebox.component";
+import { FiltreLocationComponent } from "../../components/filtre-location/filtre-location.component";
 
 @Component({
   selector: "app-location",
   standalone: true,
-  imports: [TitleboxComponent],
+  imports: [TitleboxComponent, FiltreLocationComponent],
   templateUrl: "./location.page.html",
   styleUrl: "./location.page.scss",
 })
