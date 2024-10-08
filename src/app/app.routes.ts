2647c6205af6384594b12dc9f89d158dafe52a5b
CedricDLMS <cdelmas@monchasseurimmo.com>
2024-10-08 13:48:13 +0200

    ajout routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,40 @@
-
-import { Routes } from '@angular/router';
-import { AuthGuardService } from '../services/security/auth-guard.service';
-import { MainLayoutComponent } from './layouts/main/main.layout';
-import { HomePage } from './pages/home/home.page';
-import { RoleGuardService } from '../services/security/role-guard.service';
-import { AdminHomePageComponent } from './admin-pages/admin-home/admin-home.page';
-import { NotAdminComponent } from './admin-pages/not-admin/not-admin.page';
-import { VehiculesListComponent } from './components/vehicules-list/vehicules-list.component';
-import { CarouselCarsComponent } from './components/carouselCars/carouselCars.component';
-import { CarouselRentComponent } from './components/carouselRent/carousel-rent.component';
-import { LoginPage } from './pages/login/login.page';
-import { RegisterPage } from './pages/register/register.page';
-import { VehiclesPage } from './pages/vehicles/vehicles.page';
+import { Routes } from "@angular/router";
+import { AuthGuardService } from "../services/security/auth-guard.service";
+import { MainLayoutComponent } from "./layouts/main/main.layout";
+import { HomePage } from "./pages/home/home.page";
+import { RoleGuardService } from "../services/security/role-guard.service";
+import { AdminHomePageComponent } from "./admin-pages/admin-home/admin-home.page";
+import { NotAdminComponent } from "./admin-pages/not-admin/not-admin.page";
+import { VehiculesListComponent } from "./components/vehicules-list/vehicules-list.component";
+import { CarouselCarsComponent } from "./components/carouselCars/carouselCars.component";
+import { CarouselRentComponent } from "./components/carouselRent/carousel-rent.component";
+import { LoginPage } from "./pages/login/login.page";
+import { RegisterPage } from "./pages/register/register.page";
+import { VehiclesPage } from "./pages/vehicles/vehicles.page";
+import { LocationPage } from "./pages/location/location.page";
 
 export const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: MainLayoutComponent,
     children: [
-      { path: '', component: HomePage, canActivate: [AuthGuardService] }, // canActivate: [AuthGuardService] pour rendre la page accessible aux connectés uniquement
-      { path: 'login', component: LoginPage },
-      { path: 'register', component: RegisterPage },
+      { path: "", component: HomePage, canActivate: [AuthGuardService] }, // canActivate: [AuthGuardService] pour rendre la page accessible aux connectés uniquement
+      { path: "login", component: LoginPage },
+      { path: "register", component: RegisterPage },
       {
-        path: 'notAdmin',
+        path: "notAdmin",
         component: NotAdminComponent,
         canActivate: [AuthGuardService],
       },
-      { path: 'createVehicle', 
+      {
+        path: "createVehicle",
         component: VehiculesListComponent,
-        canActivate: [AuthGuardService, RoleGuardService]
+        canActivate: [AuthGuardService, RoleGuardService],
       },
-      { path: 'vehicles', component: VehiclesPage },
-      { path: 'carouselCars', component: CarouselCarsComponent },
-      { path: 'carouselRent/:id', component: CarouselRentComponent },
+      { path: "vehicles", component: VehiclesPage },
+      { path: "carouselCars", component: CarouselCarsComponent },
+      { path: "carouselRent/:id", component: CarouselRentComponent },
+      { path: "locations", component: LocationPage },
       // {path: 'admin', component: AdminHomePageComponent, canActivate: [AuthGuardService, RoleGuardService]},
       // Nouvelle page (ex: rent.page.ts) ici = {path: 'rent', component: RentPageComponent, canActivate: [AuthGuardService] }
       // Une page est un composant mais on remplace .component.ts par .page.ts et le nom de la class on met namePageComponent
@@ -42,8 +44,9 @@ export const routes: Routes = [
     // canActivate: [AuthGuardService]
   },
   {
-    path: 'admin',
+    path: "admin",
     component: MainLayoutComponent,
-    children: [{ path: '', component: AdminHomePageComponent }],
+    children: [{ path: "", component: AdminHomePageComponent }],
     canActivate: [AuthGuardService, RoleGuardService],
-  }]
+  },
+];
