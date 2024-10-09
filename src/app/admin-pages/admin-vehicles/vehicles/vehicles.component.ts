ba08e8d6d41e6f3680676ff801e1c645d22dd497
“[olivierkoe]” <[olikoen@gmail.com]>
2024-10-10 01:12:50 +0200

    Mise en forme page admine vehicule

--- src/app/admin-pages/admin-vehicles/vehicles/vehicles.component.ts
+++ src/app/admin-pages/admin-vehicles/vehicles/vehicles.component.ts
@@ -1,29 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { Vehicle } from '../../../../models/Vehicle';
 import { VehiculesService } from '../../../../services/vehicules/vehicules.service';
 import { CommonModule } from '@angular/common';
 
+interface Car {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-vehicles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+  ],
   templateUrl: './vehicles.component.html',
-  styleUrl: './vehicles.component.scss'
+  styleUrl: './vehicles.component.scss',
 })
 export class VehiclesComponent {
-
   public vehicles$!: Observable<Vehicle[]>;
 
-  constructor(private _vehicleService: VehiculesService){}
+  constructor(private _vehicleService: VehiculesService) {}
 
-
-  ngOnInit(){
+  ngOnInit() {
     this.vehicles$ = this._vehicleService.loadAll$();
-    this.vehicles$.subscribe((v)=>{
-      console.log('sub',v);
-      
-    })
+    this.vehicles$.subscribe((v) => {
+      console.log('sub', v);
+    });
   }
 }
