f18a0c83ad099f00791ce7e5432716e132632821
Florian Tocco <t.florian181181@gmail.com>
2024-10-10 12:41:16 +0200

    tests

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,13 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
